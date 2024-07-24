094792aaa6dfd17537ebf02ce2b565808bb6283b
AjaySharma <145326516+ajayedugaon@users.noreply.github.com>
2024-07-24 16:45:27 +0530

    payment screen is added sdg

--- foodgeo_partner/test/widget_test.dart
+++ foodgeo_partner/test/widget_test.dart
@@ -7,7 +7,7 @@
 
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
-import 'package:foodgeo_partner/Screen/user_register.dart';
+import 'package:foodgeo_partner/main.dart';
 
 
 void main() {
