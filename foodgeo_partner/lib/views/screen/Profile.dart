8c65a909ce8e6406b180858b84289fda274452e8
saifali9876 <saifali50272@gmail.com>
2024-09-01 16:04:29 +0530

    code changing

--- foodgeo_partner/lib/views/screen/Profile.dart
+++ foodgeo_partner/lib/views/screen/Profile.dart
@@ -1,7 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:foodgeo_partner/views/screen/phone_verification_screen.dart';
 import 'package:foodgeo_partner/views/screen/profile_edit_screen.dart';
-import 'package:foodgeo_partner/views/screen/restaurant_register_screen.dart';
 import 'package:google_fonts/google_fonts.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
