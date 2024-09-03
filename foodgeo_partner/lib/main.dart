44816e99be0596d3a3dfc1cc279f6c5215897bba
Deepak kumar <deepakedugaon841215@gmail.com>
2024-09-03 12:13:11 +0530

    all are cod added

--- foodgeo_partner/lib/main.dart
+++ foodgeo_partner/lib/main.dart
@@ -1,17 +1,15 @@
-// import 'package:flutter/material.dart';
-//
-// import 'Screen/splash_screen.dart';
-// void main(){
-//   runApp(MyApp());
-// }
-// class MyApp extends StatelessWidget {
-//   const MyApp({super.key});
-//
-//   @override
-//   Widget build(BuildContext context) {
-//     return MaterialApp(
-//         debugShowCheckedModeBanner: false,
-//         home: SplashScreen()
-//     );
-//   }
-// }
+import 'package:flutter/material.dart';
+void main(){
+  runApp(MyApp());
+}
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+        debugShowCheckedModeBanner: false,
+        home: SplashScreen()
+    );
+  }
+}
